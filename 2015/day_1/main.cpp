1f2a2424e98b8201fe08e2110c7f886fc02927dc
WilliamPyke <william.pyke1@my.uwi.edu>
2022-12-31 19:58:23 -0400

    changed the order of code output

--- 2015/day_1/main.cpp
+++ 2015/day_1/main.cpp
@@ -3,8 +3,9 @@
 
 class Calcs {
 public:
-    int calcFloor(std::vector<char>& charFile) {
+    int calcFloorAndCharPos(std::vector<char>& charFile) {
         int floor = 0;
+        int char_pos = 0;
         bool basement = false;
         for(int i = 0; i < charFile.size(); i++) {
             if (charFile[i] == '(') {
@@ -14,20 +15,21 @@ public:
             }
             if (floor == -1 && basement == false) {
                 basement = true;
-                std::cout << "Santa first enters the basement at character position: " << i+1 << '.' << std::endl;
+                char_pos = i+1;
             } 
         }
-        return floor;
+        std::cout << "The instructions take Santa to floor: " << floor << '.' << std::endl;
+        return char_pos;
     };
 };
 
 int main() {
     Calcs calc;
     Utils utils;
-    int floor;
+    int char_pos;
     std::string filepath = "/Users/williampyke/Documents/Code/AOC/advent-of-code/2015/day_1/input.txt";
     std::vector<char> charFile = utils.readCharFile(filepath);
-    floor = calc.calcFloor(charFile);
-    std::cout << "The instructions take Santa to floor: " << floor << '.' << std::endl;
+    char_pos = calc.calcFloorAndCharPos(charFile);
+    std::cout << "Santa first enters the basement at character position: " << char_pos << '.' << std::endl;
     return 0;
 }
